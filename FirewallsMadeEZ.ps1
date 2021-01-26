b31fcb0eda919980206ea3ad88e6e381899bdef3
Anthony Magrene <ekfhalomaster@gmail.com>
2021-01-25 22:26:42 -0500

    Initial Commit
    -added the code

---
+++ FirewallsMadeEZ.ps1
@@ -0,0 +1,62 @@
+﻿write-host "----------------------QUICK-N-EZ FIREWALL RULE CREATOR----------------------"
+write-host "Enter Rule name:"
+$input = read-host
+$ruleName = $input
+
+write-host "Direction? Inbound(I), Outbound(O), Both(B)"
+$input = read-host
+$direction = $input
+
+write-host "Port Number?"
+$input = read-host
+$port = $input
+
+write-host "TCP(T) or UDP?"
+$input = read-host
+$proto = $input
+
+write-host "Block(B) or Allow(A)?"
+$input = read-host
+$action = $input
+
+if($direction -eq "I" -and $proto -eq "TCP" -and $action -eq "Block"){
+    New-NetFirewallRule -DisplayName $ruleName -Direction Inbound -LocalPort $port -Protocol TCP -Action Block
+}
+elseif($direction -eq "O" -and $proto -eq "TCP" -and $action -eq "Block"){
+    New-NetFirewallRule -DisplayName $ruleName -Direction Outbound -LocalPort $port -Protocol TCP -Action Block
+}
+elseif($direction -eq "B" -and $proto -eq "TCP" -and $action -eq "Block"){
+    New-NetFirewallRule -DisplayName $ruleName -Direction Outbound -LocalPort $port -Protocol TCP -Action Block
+    New-NetFirewallRule -DisplayName $ruleName -Direction Outbound -LocalPort $port -Protocol TCP -Action Block
+}
+elseif($direction -eq "I" -and $proto -eq "TCP" -and $action -eq "Allow"){
+
+}
+elseif($direction -eq "O" -and $proto -eq "TCP" -and $action -eq "Allow"){
+
+}
+elseif($direction -eq "B" -and $proto -eq "TCP" -and $action -eq "Allow"){
+
+}
+elseif($direction -eq "I" -and $proto -eq "UDP" -and $action -eq "Allow"){
+
+}
+elseif($direction -eq "O" -and $proto -eq "UDP" -and $action -eq "Allow"){
+
+}
+elseif($direction -eq "B" -and $proto -eq "UDP" -and $action -eq "Allow"){
+
+}
+elseif($direction -eq "I" -and $proto -eq "UDP" -and $action -eq "Block"){
+
+}
+elseif($direction -eq "O" -and $proto -eq "UDP" -and $action -eq "Block"){
+
+}
+elseif($direction -eq "B" -and $proto -eq "UDP" -and $action -eq "Block"){
+
+}
+
+
+
+
