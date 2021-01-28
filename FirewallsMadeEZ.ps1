8cac942ab48fddf3730dd0e0ae25a14a921f19aa
Anthony Magrene <ekfhalomaster@gmail.com>
2021-01-27 21:23:10 -0500

    Updated Input Process
    -Updated input process so user has to type less.

--- FirewallsMadeEZ.ps1
+++ FirewallsMadeEZ.ps1
@@ -11,7 +11,7 @@ write-host "Port Number?"
 $input = read-host
 $port = $input
 
-write-host "TCP(T) or UDP?"
+write-host "TCP(T) or UDP (U)?"
 $input = read-host
 $proto = $input
 
@@ -19,43 +19,43 @@ write-host "Block(B) or Allow(A)?"
 $input = read-host
 $action = $input
 
-if($direction -eq "I" -and $proto -eq "TCP" -and $action -eq "Block"){
+if($direction -eq "I" -and $proto -eq "T" -and $action -eq "B"){
     New-NetFirewallRule -DisplayName $ruleName + "Inbound"-Direction Inbound -LocalPort $port -Protocol TCP -Action Block
 }
-elseif($direction -eq "O" -and $proto -eq "TCP" -and $action -eq "Block"){
+elseif($direction -eq "O" -and $proto -eq "T" -and $action -eq "B"){
     New-NetFirewallRule -DisplayName $ruleName + "Outbound"-Direction Outbound -LocalPort $port -Protocol TCP -Action Block
 }
-elseif($direction -eq "B" -and $proto -eq "TCP" -and $action -eq "Block"){
+elseif($direction -eq "B" -and $proto -eq "T" -and $action -eq "B"){
     New-NetFirewallRule -DisplayName $ruleName + "Inbound" -Direction Inbound -LocalPort $port -Protocol TCP -Action Block
     New-NetFirewallRule -DisplayName $ruleName + "Outbound" -Direction Outbound -LocalPort $port -Protocol TCP -Action Block
 }
-elseif($direction -eq "I" -and $proto -eq "TCP" -and $action -eq "Allow"){
+elseif($direction -eq "I" -and $proto -eq "T" -and $action -eq "A"){
     New-NetFirewallRule -DisplayName $ruleName + "Inbound" -Direction Inbound -LocalPort $port -Protocol TCP -Action Allow
 }
-elseif($direction -eq "O" -and $proto -eq "TCP" -and $action -eq "Allow"){
+elseif($direction -eq "O" -and $proto -eq "T" -and $action -eq "A"){
     New-NetFirewallRule -DisplayName $ruleName + "Outbound" -Direction Outbound -LocalPort $port -Protocol TCP -Action Allow
 }
-elseif($direction -eq "B" -and $proto -eq "TCP" -and $action -eq "Allow"){
+elseif($direction -eq "B" -and $proto -eq "TCP" -and $action -eq "A"){
     New-NetFirewallRule -DisplayName $ruleName + "Inbound" -Direction Inbound -LocalPort $port -Protocol TCP -Action Allow
     New-NetFirewallRule -DisplayName $ruleName + "Outbound" -Direction Outbound -LocalPort $port -Protocol TCP -Action Allow
 }
-elseif($direction -eq "I" -and $proto -eq "UDP" -and $action -eq "Allow"){
+elseif($direction -eq "I" -and $proto -eq "U" -and $action -eq "A"){
     New-NetFirewallRule -DisplayName $ruleName + "Inbound" -Direction Inbound -LocalPort $port -Protocol UDP -Action Allow
 }
-elseif($direction -eq "O" -and $proto -eq "UDP" -and $action -eq "Allow"){
+elseif($direction -eq "O" -and $proto -eq "U" -and $action -eq "A"){
     New-NetFirewallRule -DisplayName $ruleName + "Outbound" -Direction Outbound -LocalPort $port -Protocol UDP -Action Allow
 }
-elseif($direction -eq "B" -and $proto -eq "UDP" -and $action -eq "Allow"){
+elseif($direction -eq "B" -and $proto -eq "U" -and $action -eq "A"){
     New-NetFirewallRule -DisplayName $ruleName + "Inbound" -Direction Inbound -LocalPort $port -Protocol UDP -Action Allow
     New-NetFirewallRule -DisplayName $ruleName + "Outbound" -Direction Outbound -LocalPort $port -Protocol UDP -Action Allow
 }
-elseif($direction -eq "I" -and $proto -eq "UDP" -and $action -eq "Block"){
+elseif($direction -eq "I" -and $proto -eq "U" -and $action -eq "B"){
     New-NetFirewallRule -DisplayName $ruleName + "Inbound" -Direction Inbound -LocalPort $port -Protocol UDP -Action Block
 }
-elseif($direction -eq "O" -and $proto -eq "UDP" -and $action -eq "Block"){
+elseif($direction -eq "O" -and $proto -eq "U" -and $action -eq "B"){
     New-NetFirewallRule -DisplayName $ruleName + "Inbound" -Direction Inbound -LocalPort $port -Protocol UDP -Action Block
 }
-elseif($direction -eq "B" -and $proto -eq "UDP" -and $action -eq "Block"){
+elseif($direction -eq "B" -and $proto -eq "U" -and $action -eq "B"){
     New-NetFirewallRule -DisplayName $ruleName + "Inbound" -Direction Inbound -LocalPort $port -Protocol UDP -Action Block
 }
 
